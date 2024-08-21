19bbb869754fa155a86cdbdcfd00e08dcde3b46c
230500226 <125464717+230500226@users.noreply.github.com>
2024-08-21 11:22:08 +0200

    init netbeans

---
+++ src/main/java/com/mycompany/javaassignmentrecursion/JavaAssignmentRecursion.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.javaassignmentrecursion;
+
+/**
+ *
+ * @author ldxt460s
+ */
+public class JavaAssignmentRecursion {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
